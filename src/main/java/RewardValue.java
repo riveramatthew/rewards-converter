4b7b2fd7dc4f06cc46176fc0f84850983a6053ae
johnriverz <juan.jose.rios.jr@gmail.com>
2023-08-16 13:20:25 -0700

    Create RewardValue.java and edit RewardsConverter.java
    Write simple constructor for cash values/miles and a skeleton for getCashValue() and getMilesValue(). Close scanner in RewardsConverter.java to prevent memory leak.

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,28 @@
+public class RewardValue {
+    double cashValue;
+    int milesValue;
+
+    public RewardValue(double theCashValue) {
+        this.cashValue = theCashValue;
+    }
+
+    public RewardValue(int theMilesValue) {
+        this.milesValue = theMilesValue;
+    }
+
+    double getCashValue(){
+
+
+        return 0;
+    }
+
+    double getMilesValue() {
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -7,14 +7,19 @@ public class RewardsConverter {
         System.out.println("Please enter a cash value to convert to airline miles: ");
         var input_value = scanner.nextLine();
         double cashValue;
+
         try {
             cashValue = Double.parseDouble(input_value);
         } catch (NumberFormatException exception) {
             System.out.println("Could not parse input value as a double, exiting");
+            scanner.close();
             return;
         }
+
         System.out.println("converting $" + input_value + " to miles");
         var rewardsValue = new RewardValue(cashValue);
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
+
+       scanner.close();
     }
 }
