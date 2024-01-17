cf56b85200385e29c8a8d63de17c4250b3334aaa
Jessica Sampaio-Herlitz <102844641+jessampaio@users.noreply.github.com>
2024-01-17 14:36:11 -0500

    add RewardValue class

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,31 @@
+public class RewardValue {
+
+  private double cashVal;
+  private double milesVal;
+
+  public RewardValue(double cashVal) {
+      this.cashVal = cashVal;
+      this.milesVal = convertCashToMiles(cashVal);
+  }
+
+  public RewardValue(double milesVal, boolean isMiles) {
+      this.milesVal = milesVal;
+      this.cashVal = convertMilesToCash(milesVal);
+  }
+
+  public double getCashValue() {
+      return cashVal;
+  }
+
+  public double getMilesValue() {
+      return milesVal;
+  }
+
+  private double convertCashToMiles(double cashVal) {
+      return cashVal / 0.0035;
+  }
+
+  private double convertMilesToCash(double milesVal) {
+      return milesVal * 0.0035;
+  }
+}
