b899f4fb076ee42d30c6232600e2332c615dac57
josiah <josiah.chapman@gmail.com>
2024-03-15 19:22:40 -0400

    init

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -4,8 +4,8 @@ public class RewardValue {
     public RewardValue(double cashvalue) {
         this.cashvalue = cashvalue;
         this.milesvalue = cashvalue/0.0035;
-       }
-public RewardValue(int milesvalue) {
+    }
+    public RewardValue(int milesvalue) {
         this.milesvalue = milesvalue;
         this.cashvalue = cashvalue*0.0035;
 
--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -7,15 +7,15 @@ public class RewardValueTests {
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
-        assertEquals(cashValue, rewardValue.getCashValue());
+        var RewardValue = new RewardValue(cashValue);
+        assertEquals(cashValue, RewardValue.getCashValue());
     }
 
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
-        assertEquals(milesValue, rewardValue.getMilesValue());
+        var RewardValue = new RewardValue(milesValue);
+        assertEquals(milesValue, RewardValue.getMilesValue());
     }
 
     @Test
