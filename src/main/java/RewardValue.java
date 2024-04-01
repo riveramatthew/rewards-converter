d1b810289a3df708ec7a9d343bc3c6587b7a60b0
Fayzulla-Shamsiev <129758929+Fayzulla-Shamsiev@users.noreply.github.com>
2024-04-01 17:47:59 +0100

    added rewardvalue class

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,21 @@
+public class RewardValue {
+    private double cashValue;
+    private int milesValue;
+
+    public RewardValue(double cash) {
+        this.cashValue = cash;
+        this.milesValue = (int) (cash / 0.0035);
+    }
+    public RewardValue(int miles){
+        this.milesValue = miles;
+        this.cashValue = miles * 0.0035;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return milesValue;
+    }
+}
