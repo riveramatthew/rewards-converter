9918eac6026fcfd2928fc928f4f3a5964d441b1c
Tim Crawley Jr <timcrawley@Tims-MacBook-Pro.local>
2024-04-05 15:54:46 -0700

    added documentation and cleaned code

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,21 +1,55 @@
+/**
+ * A class for RewardValue which will convert miles into a cash value and visa versa.
+ */
 public class RewardValue {
   private int miles;
   private double cashValue;
   private double conversion_rate = 0.0035;
 
+  /**
+   * Creates a RewardValue object given a cash value.
+   * 
+   * @param cash a cash value of type double
+   */
   public RewardValue(double cash) {
     this.cashValue = cash;
     this.miles = (int)(this.getCashValue() * (1/conversion_rate));
   }
+
+  /**
+   * Creates a RewardValue object given an integer for miles.
+   * 
+   * @param miles a miles value of type int
+   */
   public RewardValue(int miles) {
     this.miles = miles;
     this.cashValue = this.getMilesValue() * conversion_rate;
-
   }
 
-  public double getCashValue() {return this.cashValue;}
-  public int getMilesValue() {return this.miles;}
+  /**
+   * Getter method for the object's cash value.
+   * 
+   * @return the cash value of type double
+   */
+  public double getCashValue() {
+    return this.cashValue;
+  }
 
+  /**
+   * Getter method for the object's miles value.
+   * 
+   * @return the miles value of type int
+   */
+  public int getMilesValue() {
+    return this.miles;
+  }
+
+  /**
+   * Setter method for the object's miles value.
+   * 
+   * @param miles an updated value for miles
+   * @return a boolean indicating a successful update of the miles value.
+   */
   public boolean setMilesValue(int miles) {
     if (miles < 0) {
       return false;
@@ -24,6 +58,12 @@ public class RewardValue {
     return true;
   }
 
+  /**
+   * Setter method for the object's cash value.
+   * 
+   * @param cash an updated value for cash
+   * @return a boolean indicating a successful update of the cash value.
+   */
   public boolean setCashValue(double cash) {
     if (cash < 0) {
       return false;
