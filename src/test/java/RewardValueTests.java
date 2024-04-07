1a084b8b0364b26883a6a778b208e3e3fec4b248
Priyanshu147 <priyanshu035p@gmail.com>
2024-04-07 11:38:23 +0530

    updated

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,16 +20,14 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
         double cashValue = 100;
-        int milesValue = (int)(cashValue / .0035);
+        int milesValue = (int) (cashValue / .0035);
         var rewardValue = new RewardValue(cashValue);
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
         int milesValue = 10000;
         double cashValue = milesValue * .0035;
         var rewardValue = new RewardValue(milesValue);
