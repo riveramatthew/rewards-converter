6da6e9033411652b5a2a0a8d7191ad610283caa6
Bhavini Singh <bhavinisingh02@gmail.com>
2024-04-16 13:12:10 +0530

    Class Added

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,4 +1,38 @@
 import java.util.Scanner;
+class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashToMiles(cashValue);
+    }
+
+    public RewardValue(double milesValue, boolean isMiles) {
+        if (isMiles) {
+            this.milesValue = milesValue;
+            this.cashValue = milesToCash(milesValue);
+        } else {
+            this.milesValue = cashToMiles(milesValue);
+            this.cashValue = milesValue;
+        }
+    }
+
+    private double cashToMiles(double cashValue) {
+        return cashValue / 0.0035;
+    }
+
+    private double milesToCash(double milesValue) {
+        return milesValue * 0.0035;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
 
 public class RewardsConverter {
     public static void main(String[] args) {
