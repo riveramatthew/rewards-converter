afd6ed65217afaac8f6da412aca026b339fc544b
bsandeep2003 <245121735139@mvsrec.edu.in>
2024-04-20 20:55:54 +0530

    completion_of_task1

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,28 +1,21 @@
 public class RewardValue {
   private final double cashValue;
-  public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+  public static final double CONVERSION_RATE = 0.0035;
 
   public RewardValue(double cashValue) {
       this.cashValue = cashValue;
   }
 
   public RewardValue(int milesValue) {
-      this.cashValue = convertToCash(milesValue);
+      this.cashValue = milesValue * CONVERSION_RATE;
   }
 
-  private static int convertToMiles(double cashValue) {
-      return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
-  }
-
-  private static double convertToCash(int milesValue) {
-      return milesValue * MILES_TO_CASH_CONVERSION_RATE;
-  }
 
   public double getCashValue() {
       return cashValue;
   }
 
   public int getMilesValue() {
-      return convertToMiles(this.cashValue);
+      return (int) (cashValue / CONVERSION_RATE);
   }
 }
