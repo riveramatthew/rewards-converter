7a9c987aef501bbaeabe6c1f54242d903f7e24e5
Stephanie Bernades <stephaniebstephy@gmail.com>
2024-04-22 23:39:52 -0400

    Add RewardValue class with constructors and methods

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,30 @@
+public class RewardValue {
+    private double cash;
+    private int miles;
+    private static final double fixed_rate = 0.0035;
+
+    //Constructor that accepts cash value
+    RewardValue(double cash){
+        this.cash = cash;
+        this.miles = (int)(cash / fixed_rate);
+    }
+
+    //constructor that accepts miles values
+    RewardValue(int miles){
+        this.miles = miles;
+        this.cash = miles * fixed_rate;
+    }
+
+    //Method to get cash value
+   public double getCashValue(){
+        return this.cash;
+    }
+
+    //method to get miles value
+    public int getMilesValue(){
+        return this.miles;
+    }
+
+
+
+}
