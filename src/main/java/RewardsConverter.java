89481141eccd78fc4a70d283a3519eba35d6060b
prinzthom <106787270+prinzthom@users.noreply.github.com>
2024-05-01 00:04:26 +0100

    Update RewardsConverter.java

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,28 +1,20 @@
-public class RewardValue {
-    private final double cashValue;
-    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+import java.util.Scanner;
 
-    public RewardValue(double cashValue) {
-        this.cashValue = cashValue;
-    }
-
-    public RewardValue(int milesValue) {
-        this.cashValue = convertToCash(milesValue);
-    }
-
-    private static int convertToMiles(double cashValue) {
-        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
-    }
-
-    private static double convertToCash(int milesValue) {
-        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
-    }
-
-    public double getCashValue() {
-        return cashValue;
-    }
-
-    public int getMilesValue() {
-        return convertToMiles(this.cashValue);
+public class RewardsConverter {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Credit Card Rewards Converter!");
+        System.out.println("Please enter a cash value to convert to airline miles: ");
+        var input_value = scanner.nextLine();
+        double cashValue;
+        try {
+            cashValue = Double.parseDouble(input_value);
+        } catch (NumberFormatException exception) {
+            System.out.println("Could not parse input value as a double, exiting");
+            return;
+        }
+        System.out.println("converting $" + input_value + " to miles");
+        var rewardsValue = new RewardValue(cashValue);
+        System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
     }
 }
