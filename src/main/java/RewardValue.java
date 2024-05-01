be18b9c919849f903010e61232a7ab1cc2af6498
IamAFM <168609737+IamAFM@users.noreply.github.com>
2024-04-30 22:29:50 -0400

    Update RewardValue.java

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,1 +1,27 @@
+public class RewardValue
+{
+    private double cash;
+    private int miles;
+    private double milesToCashRate = 0.0035;
+    public RewardValue(double cash)
+    {
+        this.cash = cash;
+        this.miles = (int) (cash / milesToCashRate);
+    }
 
+    public RewardValue(int miles)
+    {
+        this.miles = miles;
+        this.cash = miles * milesToCashRate;
+    }
+
+    public double getCashValue()
+    {
+        return cash;
+    }
+
+    public int getMilesValue()
+    {
+        return miles;
+    }
+}
