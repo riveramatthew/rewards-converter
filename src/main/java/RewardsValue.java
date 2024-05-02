d709d521bfaf2be41e41faa7ea1d197b6ef0b733
Pavan <pavankalyanstar5@gmail.com>
2024-05-02 10:53:10 +0530

    Final

---
+++ src/main/java/RewardsValue.java
@@ -0,0 +1,21 @@
+class RewardValue {
+    private double cashValue;
+    private int milesValue;
+    RewardValue(double cashValue){
+        this.cashValue = cashValue;
+        this.milesValue = (int)(cashValue / 0.0035);
+    }
+
+    RewardValue( int milesValue){
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+    double getCashValue(){
+        return cashValue;
+    }
+    int getMilesValue(){
+        return milesValue;
+
+    }
+
+}
