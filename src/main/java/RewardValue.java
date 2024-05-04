d72e87915f08d9fcae05e9b31a4b97996635d5ce
Emad Hussien <emadhussien851@gmail.com>
2024-05-04 04:45:13 +0300

    Writing Unit Tests for the conversion functionalities
    Writing Two Unit Tests functions to test the conversions to and from both cash and miles.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,6 +1,6 @@
 public class RewardValue {
     private final double cashValue;
-    private static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
     public RewardValue(double cash) {
         this.cashValue = cash;
     }
