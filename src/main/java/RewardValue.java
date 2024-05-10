f955b7b42c0f65da4afb927ff92540b1cdb09ed2
tejaspavanb <160583886+tejaspavanb@users.noreply.github.com>
2024-05-10 14:17:15 +0530

    Create RewardValue.java

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,26 @@
+public class RewardValue {
+    double cashValue;
+    double milesValue;
+
+    // Constructor accepting cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / 0.0035;
+    }
+
+    // Constructor accepting miles value
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+
+    // Method to get the cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Method to get the miles value
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
