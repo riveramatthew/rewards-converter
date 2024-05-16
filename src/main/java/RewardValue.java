57701cf11e86498fa3d3867ee223e28e9ecff4cb
Mohamed <mohamed.sy@compscihigh.org>
2024-05-16 13:59:55 -0400

    created a convertCashToMiles method and a convertMilesToCash method

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,13 +1,24 @@
 public class RewardValue {
     private double cash;
     private double miles;
+    private static final double milesToCashRate = 0.0035;
 
     public RewardValue(double cash) {
         this.cash = cash;
+        this.miles = convertCashToMiles(cash);
     }
 
     public RewardValue(int miles) {
         this.miles = miles;
+        this.cash = convertMilesToCash(miles);
+    }
+
+    private double convertCashToMiles(double cash) {
+        return cash / milesToCashRate;
+    }
+
+    private double convertMilesToCash(double miles) {
+        return miles * milesToCashRate;
     }
 
     public double getCashValue() {
