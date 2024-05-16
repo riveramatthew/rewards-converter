867c29856d1004705932cbe41877eaaa66c0964b
Mohamed <mohamed.sy@compscihigh.org>
2024-05-16 13:43:33 -0400

    Created two constructors that accepts cash and one that accepts miles

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,13 @@
+public class RewardValue {
+    private double cash;
+    private double miles;
+
+    public RewardValue(double cash) {
+        this.cash = cash;
+    }
+
+    public RewardValue(int miles) {
+        this.miles = miles;
+    }
+
+}
