23c92cac5f7681a7c88be636db4cfa67bd2717bf
Mohamed <mohamed.sy@compscihigh.org>
2024-05-16 14:36:58 -0400

    attempting to create pull request

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -22,6 +22,7 @@ public class RewardValueTests {
     void convert_from_cash_to_miles() {
         RewardValue rewardValue = new RewardValue(100.0);
         double expectedMiles = 100.0 / 0.0035;
+
         assertEquals(expectedMiles, rewardValue.getMilesValue(), 0.0001);
     }
 
@@ -29,6 +30,7 @@ public class RewardValueTests {
     void convert_from_miles_to_cash() {
         RewardValue rewardValue = new RewardValue(100);
         double expectedCash = 0.0001 * 0.0035;
+
         assertEquals(expectedCash, rewardValue.getCashValue(), 100.0);
     }
 }
