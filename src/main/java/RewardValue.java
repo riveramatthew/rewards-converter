58ea98249e82316539c27fca1b972fe3bfcc72f2
Mohamed <mohamed.sy@compscihigh.org>
2024-05-16 14:09:48 -0400

    added comments

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,4 +1,6 @@
 public class RewardValue {
+    // this class can either take in miles or cash. if cash was given it is then converted into miles
+    // if miles was given it will convert it to cash
     private double cash;
     private double miles;
     private static final double milesToCashRate = 0.0035;
