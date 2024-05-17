8d83301646d4c215e5b6f1b7f655a9ec4c2a6d81
Vaibhavi <porevaibhavi5@gmail.com>
2024-05-17 12:50:18 +0530

    add unit tests

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -5,11 +5,19 @@ public class RewardValue {
         this.cashValue = cashValue;
     }
 
+    RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
     public double getCashValue() {
         return cashValue;
     }
 
     public double getMilesValue() {
         return cashValue/0.0035;
+    }
+
+    public double convertToCash(int milesValue) {
+        return milesValue*0.0035;
     }
 }
