b35c3e85d8c838b81a7696dc2bb880a3a8150f75
DRILLAXB <dilax511@hotmail.com>
2024-05-20 17:44:41 +0100

    RewardValue class

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,28 @@
+
+public class RewardValue {
+	private double cashValue;
+	private int mileValue;
+	
+	public RewardValue(double cashValue) {
+		this.cashValue = cashValue;
+	}
+	
+	public RewardValue(int mileValue) {
+		this.mileValue = mileValue;
+		
+	}
+	
+	public double getCashValue() {
+		return cashValue;
+	}
+	
+	public int getMilesValue() {
+		return mileValue;
+	}
+	
+	public double convertMileToCash(int miles) {
+		double cash = miles*0.0035;
+		return cash;
+	}
+
+}
