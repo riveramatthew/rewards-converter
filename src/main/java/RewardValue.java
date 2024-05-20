004198a382f2859aae914a8f7b0910d96eb45ba0
22tushar <tushargehlot123@gmail.com>
2024-05-20 13:05:01 +0530

    implemented RewardValue class for value conversion

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,29 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+    private static final double CONVERSION_RATE = 0.0035;
+
+    // Constructor that accepts a cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / CONVERSION_RATE;
+    }
+
+    // Constructor that accepts a value in miles
+    public RewardValue(double milesValue, boolean isMiles) {
+        if (isMiles) {
+            this.milesValue = milesValue;
+            this.cashValue = milesValue * CONVERSION_RATE;
+        }
+    }
+
+    // Method to get the cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Method to get the miles value
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
