16115f8f92f8a8ad8d151665e28d55f019239f7a
Vishal Pandey <bishalpandey2001@gmail.com>
2024-05-24 16:22:46 +0530

    changes-reward-value

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,5 +1,34 @@
 import java.util.Scanner;
 
+public class RewardValue {
+    private double cashValue;
+    public static final double CONVERSION_RATE = 0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int milesValue) {
+        this.cashValue = converttoCash(milesValue);
+    }
+
+    private static int converttoMiles(double cashValue) {
+        return (int) (cashValue/CONVERSION_RATE);
+    }
+
+    private static double converttoCash(int milesValue) {
+        return milesValue * CONVERSION_RATE;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return converttoMiles(this.cashValue);
+    }
+}
+
 public class RewardsConverter {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
