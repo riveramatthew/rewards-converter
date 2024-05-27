f77b87921f54a5b043359b684467c20bcaccaa74
3Roland59 <amarteyroland359@gmail.com>
2024-05-27 23:57:38 +0100

    Added RewardValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -2,28 +2,32 @@ public class RewardValue {
     
     private static final double MILES_TO_CASH_RATE = 0.0035;
     private double cashValue;
-    private double milesValue;
+    private int milesValue;
     
     RewardValue(double cash) {
         cashValue = cash;
-        milesValue = cashValue / MILES_TO_CASH_RATE;
+        milesValue = convertCashToMiles(cashValue);
     }
 
 
-    RewardValue(double miles, boolean isMiles) {
-        if (isMiles) {
+    RewardValue(int miles) {
             milesValue = miles;
-            cashValue = milesValue * MILES_TO_CASH_RATE;
-        } else {
-            throw new IllegalArgumentException("Use the other constructor for cash conversion");
-        }
+            cashValue = convertMilesToCash(milesValue);
     }
 
     public double getCashValue() {
         return cashValue;
     }
 
-    public double getMilesValue() {
+    public int getMilesValue() {
         return milesValue;
+    }
+
+    private int convertCashToMiles(double cash) {
+        return (int)(cash / MILES_TO_CASH_RATE);
+    }
+
+    private double convertMilesToCash(int miles) {
+        return miles * MILES_TO_CASH_RATE;
     }
 }
