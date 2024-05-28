2420c66f37fe6e1ae3c539db30aa104dd0b5ccaa
Masham Siddiqui <110716470+mashamegul@users.noreply.github.com>
2024-05-29 04:25:57 +1000

    Updated Unit Tests to be in line with updates onto RewardValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    public RewardValue(double cash) {
+        this.cashValue = cash;
+        this.milesValue = cash / 0.0035;
+    }
+
+    public RewardValue (int miles) {
+        this.milesValue = miles;
+        this.cashValue = miles * 0.0035;
+    }
+
+    public double getCashValue() {
+        return this.cashValue;
+    }
+
+    public double getMilesValue() {
+        return this.milesValue;
+    }
 }
