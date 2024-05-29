df97e4fa52b4b8e6c76d6a0c452f8c3458f5a598
jdn24 <mail.jadenade@gmail.com>
2024-05-30 00:52:50 +0400

    updated and tested

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -2,15 +2,16 @@ public class RewardValue {
 
     private double milesValue;
     private double cashValue;
+    private double CONVERSION_RATE=0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = cashValue * 0.0035;
+        this.milesValue = cashValue * CONVERSION_RATE;
     }
 
     public RewardValue(int milesValue) {
         this.milesValue = milesValue;
-        this.cashValue = milesValue / 0.0035;
+        this.cashValue = milesValue / CONVERSION_RATE;
     }
 
     public RewardValue(double cashValue, double milesValue) {
