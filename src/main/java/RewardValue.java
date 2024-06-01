dedd3ad7298962ed6b7f96897d3a355874993418
saikhobragade <salonikhobragade2411@gmail.com>
2024-06-01 15:14:04 +0530

    Updated RewardValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,17 +1,17 @@
 public class RewardValue {
 
-    final double cashValue;
-     double milesValue;
+     final double cashValue;
+     final double milesValue;
 
     public RewardValue(double cashValue){
         this.cashValue = cashValue;
         this.milesValue = cashValue / 0.0035;
     }
 
-   /* public RewardValue(double milesValue){
+   public RewardValue(int milesValue){
         this.milesValue = milesValue;
         this.cashValue = milesValue * 0.0035;
-    } */
+    }
 
     public double getCashValue() {
         return cashValue;
