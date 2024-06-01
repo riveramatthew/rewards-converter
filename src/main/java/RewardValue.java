4f42ee94fc3148584dd0339bbedc73ed7babc59e
saikhobragade <salonikhobragade2411@gmail.com>
2024-06-01 14:55:12 +0530

    Added RewardValue class for converting cash to miles

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+
+    final double cashValue;
+     double milesValue;
+
+    public RewardValue(double cashValue){
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / 0.0035;
+    }
+
+   /* public RewardValue(double milesValue){
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    } */
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
+    }
 }
