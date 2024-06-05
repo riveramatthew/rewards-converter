83af3849cf9c3cb0448ef4f5af612451e06614fe
Saurav Kumar <sauravcoc2@gmail.com>
2024-06-05 16:58:52 +0530

    Update RewardValueTests.java
    Generated Test Cases

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,15 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 35000;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(cashValue, rewardValue.getCashValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 3750;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(milesValue, rewardValue.getMilesValue());
     }
 }
