402c599ce3c32149b42cc92b4dc23baff6c48b91
sarang2503 <sarangzirpe97@gmail.com>
2024-06-08 03:05:20 +0530

    Your descriptive commit message here

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    private double cash;
+    private double mile;
+
+
+
+    public RewardValue(double cash) {
+        this.cash = cash;
+        this.mile=this.cash*0.0035;
+    }
+
+    public double getMilesValue() {
+        return this.mile;
+    }
+    public double getCashValue(){
+        return this.cash;
+    }
 }
