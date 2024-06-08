ecfe0bbb04877e9ac699c247ba1882832b38df31
sarang2503 <sarangzirpe97@gmail.com>
2024-06-08 20:39:54 +0530

    succesfully

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,12 +1,16 @@
 public class RewardValue {
     private double cash;
-    private double mile;
-
+    private int mile;
 
 
     public RewardValue(double cash) {
         this.cash = cash;
-        this.mile=this.cash*0.0035;
+        this.mile = (int)(cash*0.0035);
+    }
+    public RewardValue(int mile) {
+        this.mile=mile;
+        this.cash= (mile/0.0035);
+
     }
 
     public double getMilesValue() {
@@ -14,5 +18,13 @@ public class RewardValue {
     }
     public double getCashValue(){
         return this.cash;
+    }
+    public double cashtomile(double cash){
+        RewardValue temp= new RewardValue(cash);
+        return temp.getMilesValue();
+    }
+    public double miletocash(int mile){
+        RewardValue temp= new RewardValue(mile);
+        return temp.getCashValue();
     }
 }
