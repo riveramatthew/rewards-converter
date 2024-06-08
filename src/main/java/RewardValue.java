03c703888582c72d298884dcc638d86128622676
Ayush Deep <ayushdeep2001@gmail.com>
2024-06-08 23:42:13 +0530

    Task 2

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,19 +1,22 @@
 public class RewardValue {
-    private Double cash;
+    private double cash;
     private int miles;
+    private int toggle;
 
     public RewardValue(Double cash){
         this.cash = cash;
+        this.toggle = 0;
     }
     public RewardValue(int miles){
         this.miles = miles;
+        this.toggle = 1;
     }
 
     public int getMilesValue(){
-        return (int) (cash / 0.0035);
+        return toggle==1?miles:(int) (cash / 0.0035);
     }
 
     public Double getCashValue(){
-        return miles * 0.0035;
+        return toggle==0?cash:miles * 0.0035;
     }
 }
