eda7eb3e04142577717350195423a19e9626cadb
sarang2503 <sarangzirpe97@gmail.com>
2024-06-08 20:28:48 +0530

    addes

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,18 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+
+        double cash=100000.0;
+        RewardValue temp1= new RewardValue(cash);
+        assertEquals(temp1.getMilesValue(),temp1.cashtomile(cash));
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+
+        int miles=100;
+        RewardValue temp2= new RewardValue(miles);
+        assertEquals(temp2.getCashValue(),temp2.miletocash(miles));
+
     }
 }
