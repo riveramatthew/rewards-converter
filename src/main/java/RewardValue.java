6fec6a4f08182b6b3338df971a30aa7cf57db790
Sushree Manaswini Biswal <biswalsushree000@gmail.com>
2024-06-11 13:48:10 +0530

    Task-1 done

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -3,29 +3,24 @@ public class RewardValue {
     private double milesValue;
     private static final double MILES_TO_CASH_RATE = 0.0035;
 
-    // Constructor that accepts a cash value
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
         this.milesValue = cashValue / MILES_TO_CASH_RATE;
     }
 
-    // Constructor that accepts a value in miles
     public RewardValue(int milesValue) {
         this.milesValue = milesValue;
         this.cashValue = milesValue * MILES_TO_CASH_RATE;
     }
 
-    // Method to get the cash value of the RewardValue
     public double getCashValue() {
         return cashValue;
     }
 
-    // Method to get the miles value of the RewardValue
     public double getMilesValue() {
         return milesValue;
     }
 
-    // Main method for testing purposes
     public static void main(String[] args) {
         RewardValue reward1 = new RewardValue(100.0); // cash value constructor
         RewardValue reward2 = new RewardValue(10000); // miles value constructor
