daa74a19e639658cabeb5ba3a282f6289e141a05
Srimathi Srinivasan <srimathi.srinivasan244@gmail.com>
2024-06-13 20:36:25 +0530

    Completed RewardValue Class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue
+{
+    public static double cashVal;
+    public static int miles;
+    RewardValue(double cashValue)
+    {
+        cashVal=cashValue;
+    }
+    RewardValue(int milesValue)
+    {
+        miles=milesValue;
+    }
+    public double getCashValue()
+    {
+        cashVal=(0.0035)*(double)miles;
+        return cashVal;
+    }
+    public int getMilesValue()
+    {
+        miles=(int)(cashVal/0.0035);
+        return miles;
+    }
 }
+
