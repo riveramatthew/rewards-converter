5a0ed64891709db42e0a43b76141e48d18405c96
Anushka Mulik <80084874+AnushkaM02@users.noreply.github.com>
2024-06-14 14:43:26 +0530

    updated reward value class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -2,28 +2,33 @@ public class RewardValue {
 
     private double cashValue;
     private double milesValue;
-    private double rate = 0.0035;
+    private static final double rate = 0.0035;
 
     // Constructor that accepts cash value
     public RewardValue(double cashValue){
         this.cashValue = cashValue;
-        this.milesValue = cashValue / rate;
     }
 
     // Constructor that accepts miles value
-    public RewardValue(double milesValue, double rate){
-        this.milesValue = milesValue;
-        this.rate = rate;
-        this.cashValue = milesValue * rate;
+    public RewardValue(int milesValue){
+        this.cashValue = toCash(milesValue);
     }
 
     // Method that returns cash value
-    public double getCashValue(){
-        return cashValue;
+    public double toCash(int milesValue){
+        return milesValue * rate;
     }
 
     // Method that returns miles value
-    public double getMilesValue(){
-        return milesValue;
+    public int toMiles(double cashValue){
+        return (int) (cashValue / rate);
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return toMiles(this.cashValue);
     }
 }
