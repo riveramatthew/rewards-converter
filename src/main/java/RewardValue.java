f12fc9306322d1ba9bbaa191e290932e8cc3074e
samabrahaam <samcabraham9@gmail.com>
2024-06-15 22:18:45 -0500

    Fixed RewardValueTests

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,28 +1,30 @@
 //package main.java;
-public class RewardValue {
-    private double cashValue;
-    private double milesValue;
 
-    // Conversion rate from miles to cash
-    private static final double MILES_TO_CASH_RATE = 0.0035;
+public class RewardValue {
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = cashValue / MILES_TO_CASH_RATE;
     }
 
-    public RewardValue(double milesValue, boolean isMiles) {
-        if (isMiles) {
-            this.milesValue = milesValue;
-            this.cashValue = milesValue * MILES_TO_CASH_RATE;
-        }
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
     }
 
     public double getCashValue() {
         return cashValue;
     }
 
-    public double getMilesValue() {
-        return milesValue;
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
 }
