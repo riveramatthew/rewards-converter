6a9040e925b55daee459d73dce4693ab3c61b1d9
Devraj Anchan <devrajanchan333@gmail.com>
2024-06-18 17:50:12 +0530

    Test Cases Added

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -28,7 +28,7 @@ public class RewardValueTests {
 
     @Test
     void convert_from_miles_to_cash() {
-        int milesValue = 10000;
+        int milesValue = 1000;
         double expectedCashValue = milesValue * RewardValue.MILES_TO_CASH_RATE;
         var rewardValue = new RewardValue(milesValue);
         assertEquals(expectedCashValue, rewardValue.getCashValue());
