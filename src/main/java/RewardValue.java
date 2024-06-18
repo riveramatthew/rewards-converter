1bfe7cdb984cce8d5a49a3cf003978ba9eeff918
Rutuj-Runwal <rutujrunwal001@gmail.com>
2024-06-18 20:23:21 +0530

    Feat: Conversion rate centralization

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,6 +1,7 @@
 public class RewardValue {
     static double cashVal;
     static int milesVal;
+    static final double CONVERSION_RATE = 0.0035;
 
     public RewardValue(double cashValue) {
         cashVal = cashValue;
@@ -11,10 +12,10 @@ public class RewardValue {
     }
 
     public static double getCashValue() {
-        return milesVal * 0.0035;
+        return milesVal * CONVERSION_RATE;
     }
 
     public int getMilesValue() {
-        return (int) (cashVal / 0.0035);
+        return (int) (cashVal / CONVERSION_RATE);
     }
 }
