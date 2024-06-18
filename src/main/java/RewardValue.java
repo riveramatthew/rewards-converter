754824f70ab51dd2a0bfea93e2c30006ccc9d227
jszama <jmszamotulski@gmail.com>
2024-06-18 13:45:57 +0100

    Created RewardValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,22 @@
 public class RewardValue {
+    double cash_value;
+    double mileage;
+
+    public RewardValue(double cash_value) {
+        this.cash_value = cash_value;
+        this.mileage = cash_value / 0.0035;
+    }
+
+    public RewardValue(int miles) {
+        this.mileage = miles;
+        this.cash_value = miles * 0.0035;
+    }
+
+    public double getCashValue(){
+        return this.cash_value;
+    }
+
+    public double getMilesValue(){
+        return this.mileage;
+    }
 }
