f31c53f058e78921aca3083f69003c74fd460307
rishimanasreddy <rishimanasreddy@gmail.com>
2024-06-20 14:11:46 -0500

    Add RewardValue class

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,15 @@
+public class RewardValue {
+    private double value;
+
+    public RewardValue(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
