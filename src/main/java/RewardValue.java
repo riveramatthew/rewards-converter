0cc40f233b515f40d36e26bff80a6a9c2c297ad6
pehoe1 <cheahpehoe0410@gmail.com>
2024-06-21 00:11:34 +0800

    Added Public Class Reward Value with the converter function miles to cash and cash to miles with return.

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,26 @@
+public class RewardValue {
+    private double cashValue;
+    private int milesValue;
+    private static double miles_to_cash_rate = 0.0035;
+    // 1 Miles = 0.0035 Cash
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = (int) (cashValue / miles_to_cash_rate);
+
+    }
+    public RewardValue(int milesValue){
+        this.milesValue= milesValue;
+        this.cashValue = milesValue* miles_to_cash_rate;
+    }
+
+    public double getCashValue()    {
+
+        return cashValue;
+    }
+    public int getMilesValue()
+    {
+        return milesValue;
+    }
+
+
+}
