512accee2bf3d1a5435cf13bd9e6606ebc0eb4cc
yukizyx <yukizyx230@gmail.com>
2024-06-20 16:01:24 +0800

    Implement RewardValue unit tests.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,24 +1,28 @@
 public class RewardValue {
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
-    double cashValue;
-    int mileValue;
-    // RewardValue must have two constructors: one that accepts a cash value and 
-    // one that accepts a value in miles.
-    RewardValue(double cash) {
-        this.cashValue = cash;
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
     }
 
-    RewardValue(int miles) {
-        this.mileValue = miles;
-        this.cashValue = miles * 0.0035;
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
     }
 
     public double getCashValue() {
         return cashValue;
     }
 
-    public int getMileValue() {
-        return mileValue;
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
-
 }
