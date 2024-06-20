ea897a19aacaa243b52157e94fb4a2b2a72f167a
zhengshihan <zhengshihan1998@outlook.com>
2024-06-20 19:21:54 +0100

    second commit

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,20 +1,28 @@
 public class RewardValue {
-    double cash;
-    int miles;
-    public RewardValue(double cash) {
-    this.cash= cash;
-    this.miles= (int)(0.0035*cash);
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
     }
-    public RewardValue(int miles) {
-        this.miles = miles;
-        this.cash= miles/0.0035;
+
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
     }
 
     public double getCashValue() {
-        return cash;
+        return cashValue;
     }
 
     public int getMilesValue() {
-        return miles;
+        return convertToMiles(this.cashValue);
     }
 }
