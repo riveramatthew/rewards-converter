a0bdfa9f292027b583147793bfdc046c4485596e
Arvind Majumder <51273142+Prainex@users.noreply.github.com>
2024-06-21 19:00:40 -0400

    Added RewardValue class with constructors and methods

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,24 @@
 public class RewardValue {
+    private double cashValue;
+    private int milesValue;
+
+    private static final double MILES_TO_CASH = 0.0035; //the rate does not change for now...
+
+    public RewardValue(double cashValue){ //accepts a value in cash
+        this.cashValue = cashValue;
+        this.milesValue = (int)(cashValue / MILES_TO_CASH);
+    }
+
+    public RewardValue (int milesValue) { //accepts a value in miles
+        this.cashValue = cashValue;
+        this.milesValue = (int)(cashValue * MILES_TO_CASH);
+    }
+
+    public double getCashValue() { //returns the cash value of the RewardValue
+        return cashValue;
+    }
+
+    public double getMilesValue() { //returns how many miles the RewardValue is worth
+        return milesValue;
+    }
 }
