e78c0f08d114f3a4f79da7dc9d6e347f6dd02022
Arvind Majumder <51273142+Prainex@users.noreply.github.com>
2024-06-21 20:11:36 -0400

    Implemented unit tests and all were passed

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,24 +1,29 @@
 public class RewardValue {
     private double cashValue;
-    private int milesValue;
-
     private static final double MILES_TO_CASH = 0.0035; //the rate does not change for now...
 
     public RewardValue(double cashValue){ //accepts a value in cash
         this.cashValue = cashValue;
-        this.milesValue = (int)(cashValue / MILES_TO_CASH);
     }
 
     public RewardValue (int milesValue) { //accepts a value in miles
-        this.cashValue = cashValue;
-        this.milesValue = (int)(cashValue * MILES_TO_CASH);
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    public static int convertToMiles (double cashValue) {
+        return (int)(cashValue / MILES_TO_CASH);
+    }
+
+    public static double convertToCash (int milesValue) {
+        return milesValue * MILES_TO_CASH;
     }
 
     public double getCashValue() { //returns the cash value of the RewardValue
+
         return cashValue;
     }
 
     public double getMilesValue() { //returns how many miles the RewardValue is worth
-        return milesValue;
+        return convertToMiles(this.cashValue);
     }
 }
