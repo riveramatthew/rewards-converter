1c30d96f9a193b515832a77b50c335cb71fea206
rishimanasreddy <rishimanasreddy@gmail.com>
2024-06-22 12:18:11 -0500

    Added method to covert from milesvalue to cash

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,12 +1,28 @@
 public class RewardValue {
-    private double cashValue;
-    private static final double MILES_CONVERSION_RATE = 0.0035; // Example conversion rate
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
     }
 
-    public double getMilesValue() {
-        return cashValue / MILES_CONVERSION_RATE;
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
 }
