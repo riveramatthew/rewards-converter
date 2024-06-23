92ec7e0e1b16e7c112a639688ff6d579f7668d43
SushiOnToast <sushbomm@gmail.com>
2024-06-23 10:54:03 +0530

    added class RewardValue

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,27 @@
+public class RewardValue {
+
+    private double cashValue;
+    private double milesValue;
+
+    private final double milesToCashRate = 0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / milesToCashRate;
+    }
+
+    public RewardValue(double milesValue, boolean isMiles) {
+        if (isMiles) {
+            this.milesValue = milesValue;
+            this.cashValue = milesValue * milesToCashRate;
+        }
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
