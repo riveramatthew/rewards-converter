41e3704daa92cc10f25f31e4b2ab862da010ef15
Ramona Ciceu <77670928+Ramona-Ciceu@users.noreply.github.com>
2024-07-02 16:52:47 +0100

    Created 2 tests
    Added 2x Tests, one for convert miles to cash and convert cash to miles.

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,29 @@
+
+
+public class RewardValue {
+    public double cashValue;
+    private double milesValue;
+    private static final double MILES_TO_CASH_RATE = 0.0035;
+
+    // Constructor that accepts a cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / MILES_TO_CASH_RATE;
+    }
+
+    // Constructor that accepts a value in miles
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * MILES_TO_CASH_RATE;
+    }
+
+    // Method to get the cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Method to get the miles value
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
