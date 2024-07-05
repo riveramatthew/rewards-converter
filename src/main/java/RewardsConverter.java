1d2e41ce15e20d2cb8903de1beb8b8708fb85bd8
darshan-22 <darshannn2003@gmail.com>
2024-07-05 18:42:36 +0530

    added the rewardValue() class

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,20 +1,42 @@
 import java.util.Scanner;
-
 public class RewardsConverter {
     public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Credit Card Rewards Converter!");
         System.out.println("Please enter a cash value to convert to airline miles: ");
-        var input_value = scanner.nextLine();
+        String inputValue = scanner.nextLine();
         double cashValue;
         try {
-            cashValue = Double.parseDouble(input_value);
+            cashValue = Double.parseDouble(inputValue);
         } catch (NumberFormatException exception) {
             System.out.println("Could not parse input value as a double, exiting");
             return;
         }
-        System.out.println("converting $" + input_value + " to miles");
-        var rewardsValue = new RewardValue(cashValue);
-        System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
+        System.out.println("Convegitagitrting $" + inputValue + " to miles");
+        RewardValue rewardsValue = new RewardValue(cashValue);
+        System.out.println("$" + inputValue + " is worth " + rewardsValue.getMilesValue() + " miles");
+    }
+}
+
+class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = convertToMiles(cashValue);
+    }
+
+    private double convertToMiles(double cashValue) {
+        // Example conversion rate: 1 dollar = 100 miles
+        return cashValue * 0.0035;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
     }
 }
