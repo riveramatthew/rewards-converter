47806c55b83122b72fe1c7cb39104e82e010581e
Henry Nguyen <nguyenchihieu178205@gmail.com>
2024-07-09 23:23:45 +0700

    Add functions + tests

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,25 +1,25 @@
 
 public class RewardValue {
+    private final double CONVERSION_RATE = 0.0035;
     double cash;
-    double miles;
 
     public RewardValue(double cash) {
         this.cash = cash;
     }
 
-    public RewardValue(float miles){
-        this.miles = miles;
+    public RewardValue(int miles){
+        this.cash = round(miles * CONVERSION_RATE, 1);;
     }
 
     public double getCashValue(){
-        return round(miles * 0.0035, 1);
+        return cash;
     }
 
     public double getMilesValue(){
-        return round(cash/0.0035, 1);
+        return round(cash/CONVERSION_RATE, 1);
     }
 
-    private static double round (double value, int precision) {
+    static double round (double value, int precision) {
         int scale = (int) Math.pow(10, precision);
         return (double) Math.round(value * scale) / scale;
     }
