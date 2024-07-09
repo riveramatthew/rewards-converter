584922d8d9f26a1028662eb99179bed731ba2aa2
Ben Connelly <143213546+bconnelly13@users.noreply.github.com>
2024-07-09 12:46:09 -0400

    Added Unit Tests for converting between cash and miles and vice versa

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,20 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cash = 150.55;
+        double CASH_TO_MILES_CONVERSION = 1 / 0.0035;
+        int expectedMiles = (int) (cash * CASH_TO_MILES_CONVERSION);
+        var rewardValue = new RewardValue(cash);
+        assertEquals(expectedMiles, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int miles = 5800;
+        double MILES_TO_CASH_CONVERSION = 0.0035;
+        double expectedCash = miles * MILES_TO_CASH_CONVERSION;
+        var rewardValue = new RewardValue(miles);
+        assertEquals(expectedCash, rewardValue.getCashValue());
+
     }
 }
