225d5c98d391b12b6c24adf6f4a5dd3f28c4ec6f
kwakuduah12 <115908616+kwakuduah12@users.noreply.github.com>
2024-07-09 16:07:57 +0000

    updated rewards.java

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,37 +1,65 @@
-public class RewardValue{
-    private double cashValue;
-    private double milesValue;
-    private static final double MILES_TO_CASH_VALUE = 0.0035;
+public class RewardValue {
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
-    public RewardValue(double cashValue){
+    public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = cashValue / MILES_TO_CASH_VALUE;
     }
-    public RewardValue(double milesValue, boolean isMiles){
-        if (isMiles){
-            this.milesValue = milesValue;
-            this.cashValue = milesValue * MILES_TO_CASH_VALUE;
-        } else {
-            this.cashValue = milesValue;
-            this.milesValue = (int) (milesValue / MILES_TO_CASH_VALUE);
-        }
+
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
     }
-    public double getCashValue(){
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    public double getCashValue() {
         return cashValue;
     }
 
-    public double getMilesValue(){
-        return milesValue;
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
-
-    public static void main(String [] args){
-        RewardValue rewardCash = new RewardValue(100.0);
-        System.out.println(rewardCash.getCashValue());
-        System.out.println(rewardCash.getMilesValue());
-
-        RewardValue rewardMiles = new RewardValue(100.0);
-        System.out.println(rewardMiles.getCashValue());
-        System.out.println(rewardMiles.getMilesValue());
-    }
-
 }
+// public class RewardValue{
+//     private double cashValue;
+//     private double milesValue;
+//     private static final double MILES_TO_CASH_VALUE = 0.0035;
+
+//     public RewardValue(double cashValue){
+//         this.cashValue = cashValue;
+//         this.milesValue = cashValue / MILES_TO_CASH_VALUE;
+//     }
+//     public RewardValue(double milesValue, boolean isMiles){
+//         if (isMiles){
+//             this.milesValue = milesValue;
+//             this.cashValue = milesValue * MILES_TO_CASH_VALUE;
+//         } else {
+//             this.cashValue = milesValue;
+//             this.milesValue = (int) (milesValue / MILES_TO_CASH_VALUE);
+//         }
+//     }
+//     public double getCashValue(){
+//         return cashValue;
+//     }
+
+//     public double getMilesValue(){
+//         return milesValue;
+//     }
+
+//     public static void main(String [] args){
+//         RewardValue rewardCash = new RewardValue(100.0);
+//         System.out.println(rewardCash.getCashValue());
+//         System.out.println(rewardCash.getMilesValue());
+
+//         RewardValue rewardMiles = new RewardValue(100.0);
+//         System.out.println(rewardMiles.getCashValue());
+//         System.out.println(rewardMiles.getMilesValue());
+//     }
+
+// }
