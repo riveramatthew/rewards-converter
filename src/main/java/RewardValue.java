ddc6d21e37ccc2e6b7f9ea08affbe1eb69e8cb31
kwakuduah12 <115908616+kwakuduah12@users.noreply.github.com>
2024-07-09 17:33:56 +0000

    Added test cases and updated rewardvalue.java

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -26,40 +26,49 @@ public class RewardValue {
         return convertToMiles(this.cashValue);
     }
 }
-// public class RewardValue{
+
+// public class RewardValue {
 //     private double cashValue;
 //     private double milesValue;
-//     private static final double MILES_TO_CASH_VALUE = 0.0035;
 
-//     public RewardValue(double cashValue){
+//     // Conversion rate from miles to cash
+//     private static final double MILES_TO_CASH_RATE = 0.0035;
+
+//     // Constructor that accepts a cash value
+//     public RewardValue(double cashValue) {
 //         this.cashValue = cashValue;
-//         this.milesValue = cashValue / MILES_TO_CASH_VALUE;
+//         this.milesValue = cashValue / MILES_TO_CASH_RATE;
 //     }
-//     public RewardValue(double milesValue, boolean isMiles){
-//         if (isMiles){
+
+//     // Constructor that accepts a value in miles
+//     public RewardValue(double milesValue, boolean isMiles) {
+//         if (isMiles) {
 //             this.milesValue = milesValue;
-//             this.cashValue = milesValue * MILES_TO_CASH_VALUE;
+//             this.cashValue = milesValue * MILES_TO_CASH_RATE;
 //         } else {
 //             this.cashValue = milesValue;
-//             this.milesValue = (int) (milesValue / MILES_TO_CASH_VALUE);
+//             this.milesValue = milesValue / MILES_TO_CASH_RATE;
 //         }
 //     }
-//     public double getCashValue(){
+
+//     // Method to get the cash value of the RewardValue
+//     public double getCashValue() {
 //         return cashValue;
 //     }
 
-//     public double getMilesValue(){
+//     // Method to get the miles value of the RewardValue
+//     public double getMilesValue() {
 //         return milesValue;
 //     }
 
-//     public static void main(String [] args){
+//     public static void main(String[] args) {
+//         // Test cases
 //         RewardValue rewardCash = new RewardValue(100.0);
-//         System.out.println(rewardCash.getCashValue());
-//         System.out.println(rewardCash.getMilesValue());
+//         System.out.println("Cash Value: " + rewardCash.getCashValue()); // 100.0
+//         System.out.println("Miles Value: " + rewardCash.getMilesValue()); // 100.0 / 0.0035
 
-//         RewardValue rewardMiles = new RewardValue(100.0);
-//         System.out.println(rewardMiles.getCashValue());
-//         System.out.println(rewardMiles.getMilesValue());
+//         RewardValue rewardMiles = new RewardValue(10000.0, true);
+//         System.out.println("Cash Value: " + rewardMiles.getCashValue()); // 10000.0 * 0.0035
+//         System.out.println("Miles Value: " + rewardMiles.getMilesValue()); // 10000.0
 //     }
-
 // }
