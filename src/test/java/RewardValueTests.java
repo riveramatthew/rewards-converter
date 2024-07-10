0dc77e12dd77b61b40c27adf5c5aaec527589e83
Leon Liman <leon.liman@uni-wuerzburg.de>
2024-07-10 12:30:42 +0200

    added tests for conversions

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
 
+    private final double conversionRate = 0.0035;
+
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
@@ -20,11 +22,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        int milesValue = (int) (cashValue / conversionRate);
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        double cashValue = milesValue * conversionRate;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(cashValue, rewardValue.getCashValue());
     }
 }
