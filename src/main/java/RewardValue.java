9f28203410ae0d2d4cdeb09b72d4aa571af080b0
Kidus-Sam <kidussamson2004@gmail.com>
2024-07-10 16:46:42 -0400

    Fixed the missing tests and added a CashtoMilesConversion variable to the RewardValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,6 +1,7 @@
 public class RewardValue {
     private double cashValue=0;
     private double milesValue=0;
+    public static final double cashValueToMiles = 0.0035;
     public RewardValue(double cashValue){
         this.cashValue = cashValue;
     }
@@ -9,12 +10,12 @@ public class RewardValue {
     }
     public double getMilesValue(){
         if(cashValue!=0)
-            milesValue = cashValue/0.0035;
+            milesValue = cashValue/cashValueToMiles;
         return milesValue;
     }
     public double getCashValue(){
         if(milesValue!=0)
-            cashValue = milesValue*0.0035;
+            cashValue = milesValue*cashValueToMiles;
         return cashValue;
     }
 }
