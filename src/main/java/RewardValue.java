6cbd54c8140ae7e620548a1fb1e559cb04af185e
Leon Liman <leon.liman@uni-wuerzburg.de>
2024-07-10 12:07:58 +0200

    added RewardValue.java

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,23 @@
+public class RewardValue {
+
+  private final double cash;
+  private final int miles;
+
+  public RewardValue(double cash) {
+    this.cash = cash;
+    this.miles = (int) (cash / 0.0035);
+  }
+
+  public RewardValue(int miles) {
+    this.miles = miles;
+    this.cash = miles * 0.0035;
+  }
+
+  public double getCashValue() {
+    return this.cash;
+  }
+
+  public int getMilesValue() {
+    return this.miles;
+  }
+}
