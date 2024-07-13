d2473a0551aca6231a812c8b6e5d5273ad9f8495
Theran <mei52@purdue.edu>
2024-07-13 13:33:29 -0300

    Update RewardValue and test case addition

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -6,11 +6,23 @@ public class RewardValue {
         this.cashValue = cashValue;
     }
 
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
     public double getCashValue() {
         return cashValue;
     }
 
     public int getMilesValue() {
+        return convertToMiles(cashValue);
+    }
+
+    public static double convertToCash(int milesValue) {
+        return milesValue * CONVERSION_VALUE;
+    }
+
+    public static int convertToMiles(double cashValue) {
         return (int) (cashValue / CONVERSION_VALUE);
     }
 }
