bf3c681b7513f14ba54dbe2c5e696f754d03f1e6
Sachin1785 <sachhidanandamahapatro@gmail.com>
2024-07-17 15:33:57 +0530

    added RewardValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+     double cashValue;
+     double milesValue;
+     double rate = 0.0035;
+
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / rate;
+    }
+
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * rate;
+    }
+
+    public double getCashValue() {
+        return this.cashValue;
+    }
+
+
+    public double getMilesValue() {
+        return this.milesValue;
+    }
 }
