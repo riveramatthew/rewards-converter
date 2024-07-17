b848c23c5b384d0c24edc418ad9f65ae23d45397
VamsiKavuri8 <vamsikavuri4@gmail.com>
2024-07-17 16:43:26 -0500

    added Reward Value class and made changes in tests

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,26 @@
+public class RewardValue {
+    private static final double MilesToCash = 0.0035;
+    public double cashValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int milesValue){
+        this.cashValue = convertMilesToCash(milesValue);
+    }
+
+    private static int convertCashToMiles(double cashValue) {
+        return (int) (cashValue / MilesToCash);
+    }
+    private static double convertMilesToCash(int milesValue) {
+        return milesValue * MilesToCash;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+    public int getMilesValue() {
+        return convertCashToMiles(this.cashValue);
+    }
+}
