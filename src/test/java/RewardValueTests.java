a33d4a7cfbcc657e714cdf698346f99119d95326
Sachin1785 <sachhidanandamahapatro@gmail.com>
2024-07-17 16:07:42 +0530

    actually fixed it

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,9 +1,7 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RewardValueTests {
-
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
@@ -20,11 +18,17 @@ class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        double expectedMiles = cashValue / 0.0035;
+        assertEquals(expectedMiles, rewardValue.getMilesValue(), 0.01, "Cash to miles conversion did not match expected value");
     }
 
     @Test
     void convert_from_miles_to_cash() {
-
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        double expectedCash = milesValue * 0.0035;
+        assertEquals(expectedCash, rewardValue.getCashValue(), 0.01, "Miles to cash conversion did not match expected value");
     }
 }
