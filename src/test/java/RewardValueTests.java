cfd9ac55f16f9cc70616729608aab0fb0909aebb
aardon0466 <aardon0466@floridapoly.edu>
2024-07-17 14:21:22 -0400

    Update RewardValueTests.java

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,19 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        //assert false;
+        double cashValue = 100;
+        double milesValue = 100/0.0035;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        //assert false;
+        double cashValue = 100*0.0035;
+        int milesValue = 100;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(cashValue, rewardValue.getCashValue());
     }
 }
