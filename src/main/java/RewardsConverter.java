b7f02876b1c714005d939efdff3cee6fc2705da5
TechShivam02 <shivamtech2024@gmail.com>
2024-07-18 13:33:09 +0530

    updated soln codebase

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,20 +1,64 @@
 import java.util.Scanner;
 
 public class RewardsConverter {
+
+    static class RewardValue {
+        private double cashValue;
+        private double milesValue;
+        private static final double MILES_TO_CASH_RATE = 0.0035;
+
+        // Constructor accepting a cash value
+        public RewardValue(double cashValue) {
+            this.cashValue = cashValue;
+            this.milesValue = cashValue / MILES_TO_CASH_RATE;
+        }
+
+        // Constructor accepting a value in miles
+        public RewardValue(double milesValue, boolean isMiles) {
+            if (isMiles) {
+                this.milesValue = milesValue;
+                this.cashValue = milesValue * MILES_TO_CASH_RATE;
+            } else {
+                this.cashValue = milesValue;
+                this.milesValue = milesValue / MILES_TO_CASH_RATE;
+            }
+        }
+
+        // Method to get the cash value
+        public double getCashValue() {
+            return cashValue;
+        }
+
+        // Method to get the miles value
+        public double getMilesValue() {
+            return milesValue;
+        }
+    }
+
+
     public static void main(String[] args) {
+
         var scanner = new Scanner(System.in);
+        
         System.out.println("Welcome to the Credit Card Rewards Converter!");
         System.out.println("Please enter a cash value to convert to airline miles: ");
+        
         var input_value = scanner.nextLine();
         double cashValue;
+
         try {
             cashValue = Double.parseDouble(input_value);
         } catch (NumberFormatException exception) {
             System.out.println("Could not parse input value as a double, exiting");
             return;
         }
+        
         System.out.println("converting $" + input_value + " to miles");
+    
         var rewardsValue = new RewardValue(cashValue);
+    
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
+
+    
     }
 }
