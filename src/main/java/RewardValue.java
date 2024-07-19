c78cfe9edf2f34103e93e2b5a22beb5c718a6464
Ajay <ajayreigns0@gmail.com>
2024-07-19 14:32:01 +0530

    more professional style writing the code

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,23 +1,28 @@
 public class RewardValue {
-    private double cashValue;
-    private int milesValue;
-    private final double RATE = 0.0035;
+    private final double cashValue;
+    public static final double RATE = 0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = (int) Math.round(cashValue / RATE);
     }
 
     public RewardValue(int milesValue) {
-        this.milesValue = milesValue;
-        this.cashValue = milesValue * RATE;
+        this.cashValue = convertToCash(milesValue);
     }
 
-    public int getMilesValue() {
-        return this.milesValue;
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * RATE;
     }
 
     public double getCashValue() {
         return this.cashValue;
+    }
+
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
 }
