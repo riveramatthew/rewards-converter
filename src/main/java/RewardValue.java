c2e72d6393abd51aa34235e913b0370f56e157d1
ZHENNI LIN <925881956@qq.com>
2024-07-21 21:43:32 +0200

    Unit testing for reward-value

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,6 +1,7 @@
 public class RewardValue {
     private double cashValue;
-    private static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+
 
     // Constructor that accepts a cash value
     public RewardValue(double cashValue) {
