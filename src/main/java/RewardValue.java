6e58ecf8b60c6766428dc9b45632a96b7cc28823
Harsh Kushwah <harshkusshwah@gmail.com>
2024-07-22 23:15:48 +0530

    reqardValue class added

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,22 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    public RewardValue(double cash){
+        this.cashValue = cash;
+        this.milesValue = cash*0.0035;
+    }
+
+//    public RewardValue(double miles){
+//        this.milesValue = miles;
+//        this.cashValue = miles/0.0035;
+//    }
+
+    public double getCashValue(){
+        return cashValue;
+    }
+
+    public double getMilesValue(){
+        return milesValue;
+    }
+}
