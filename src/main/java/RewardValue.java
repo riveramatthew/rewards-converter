4d3cec4a5afe0a9be4762efd727d6ee9c673d0c0
Harsh Kushwah <harshkusshwah@gmail.com>
2024-07-23 17:31:20 +0530

    rewardValue v2

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -4,13 +4,13 @@ public class RewardValue {
 
     public RewardValue(double cash){
         this.cashValue = cash;
-        this.milesValue = cash*0.0035;
+        this.milesValue = (int) cash*0.0035;
     }
 
-//    public RewardValue(double miles){
-//        this.milesValue = miles;
-//        this.cashValue = miles/0.0035;
-//    }
+    public RewardValue(int miles){
+        this.milesValue = miles;
+        this.cashValue =(double) miles/0.0035;
+    }
 
     public double getCashValue(){
         return cashValue;
