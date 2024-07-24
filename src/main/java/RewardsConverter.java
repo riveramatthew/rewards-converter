feeb5bc9297fb1abe79d6e27942745661017e94a
vishwajeet130 <vishwajeetlearn@gmail.com>
2024-07-24 11:07:06 +0530

    reward value class added

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,5 +1,34 @@
 import java.util.Scanner;
 
+class RewardValue {
+    // Conversion rate constants
+    private static final double MILES_PER_DOLLAR = 1 / 0.0035;
+    private static final double DOLLARS_PER_MILE = 0.0035;
+
+    private double cashValue;
+
+    // Constructor for cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    // Constructor for miles value
+    public RewardValue(double milesValue, boolean isMiles) {
+        if (isMiles) {
+            this.cashValue = milesValue * DOLLARS_PER_MILE;
+        }
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return cashValue * MILES_PER_DOLLAR;
+    }
+}
+
+
 public class RewardsConverter {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
