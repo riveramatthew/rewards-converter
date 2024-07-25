1a08c0384d54e434336935f0846ffa18c9fb7a04
Vihaan Dharia <vihaandharia04@gmail.com>
2024-07-26 01:00:33 +0530

    commit1

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,5 +1,40 @@
 import java.util.Scanner;
 
+class RewardValue{
+    private double cashValue;
+    private double milesValue;
+    private static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+
+    // Constructor for cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    // Constructor for miles value
+    public RewardValue(double milesValue, boolean isMiles) {
+        if (isMiles) {
+            this.milesValue = milesValue;
+            this.cashValue = milesValue * MILES_TO_CASH_CONVERSION_RATE;
+        } else {
+            // If not miles, it could be treated as a cash value
+            this.cashValue = milesValue;
+            this.milesValue = cashValue / MILES_TO_CASH_CONVERSION_RATE;
+        }
+    }
+
+    // Get cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Get miles value
+    public double getMilesValue() {
+        return milesValue;
+    }
+
+}
+
 public class RewardsConverter {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
