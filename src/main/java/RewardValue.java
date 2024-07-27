b62660edbede94672acc6752b7c2904087da02d5
Roland <rfisher4444@hotmail.com>
2024-07-27 00:16:34 -0700

    Added the Reward Value class with comments on what I will accomplish on my next session

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,16 @@
+public class RewardValue {
+
+
+    // This constructor will accept cash values
+    public RewardValue(){
+
+    }
+
+    // This constructor will accept miles values
+    public RewardValue(){
+
+    }
+
+
+
+}
