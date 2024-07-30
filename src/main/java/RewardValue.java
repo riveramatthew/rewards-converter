111e19d934ff57120d316287a0d0b88e41f3af4c
Roland <rfisher4444@hotmail.com>
2024-07-30 16:37:39 -0700

    Added the conversion rates in the new class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,16 +1,26 @@
 public class RewardValue {
 
+    int m;
+    double c;
 
     // This constructor will accept cash values
-    public RewardValue(){
-
+    public RewardValue(double cash){
+        super();
+        this.c = cash;
     }
 
     // This constructor will accept miles values
-    public RewardValue(){
-
+    public RewardValue(int miles){
+        super();
+        this.m = miles;
     }
 
+    public double getCashValue(){
+        return m * 0.0035;
+    }
 
+    public double getMilesValue(){
+        return c / 0.0035;
+    }
 
 }
