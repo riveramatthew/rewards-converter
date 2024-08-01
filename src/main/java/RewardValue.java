2a9105c2e48747617cee4ab6cf016e9d59913e15
JaniZone <u2103106@rajagiri.edu.in>
2024-08-01 13:55:54 +0530

    Reward converter and test cases

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,25 +1,28 @@
 public class RewardValue {
-    private double cash_value;
-    private double miles_value;
-    private static final double MILES_TO_CASH_RATE= 0.0035;
-    public RewardValue(double cash_value) {
-        this.cash_value = cash_value;
-        this.miles_value = cash_value / MILES_TO_CASH_RATE;
-    }
-    public RewardValue(double miles_value, boolean isMiles)
-    {
-        if(isMiles){
-            this.miles_value = miles_value;
-            this.cash_value = miles_value * MILES_TO_CASH_RATE;}
-        else{
-            this.miles_value = miles_value;
-            this.cash_value = miles_value / MILES_TO_CASH_RATE;}
-    }
-   public double getCashValue() {
-        return cash_value;
-   }
-   public double getMilesValue() {
-        return miles_value;
-   }
-}
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
+    }
+}
